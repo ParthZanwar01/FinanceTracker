c4a0a991cff38c0a434dd3fb0f9ffd96cc97450d
TheMathGenius01 <parth.zanwar01@gmail.com>
2025-05-14 15:16:07 -0500

    Update SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -4,5 +4,23 @@ public class SavingsAccount extends Account {
 
     public SavingsAccount(double balance) {
         super(balance);
+        interestRate = 0.01;
     }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void addInterest() {
+        double interest = getBalance() * interestRate;
+        balance += interest;
+    }
+
+    public String toString() {
+        return "Savings Account: " + getBalance() + " interest rate: " + interestRate;
+    }
+
+
+
+
 }
